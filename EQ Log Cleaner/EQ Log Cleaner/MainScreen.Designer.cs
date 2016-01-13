c6cf2bc9cf3b7f26ea2aeef826d41b9254748906
Beimeith <EQWizardB@aol.com>
2016-01-13 01:38:11 -0800

    Simple Parsing
    -Can select Log File
    -Adapted CheckFile() from GamParse
    -Adapted beginning of LoadFile from Gamparse
    -Added checks for the six major spam lines

---
+++ EQ Log Cleaner/EQ Log Cleaner/MainScreen.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace EQ_Log_Cleaner
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.B_Open_Log = new System.Windows.Forms.Button();
+            this.L_Cant_Use_Command = new System.Windows.Forms.Label();
+            this.L_Havent_Recovered_Yet = new System.Windows.Forms.Label();
+            this.L_Cant_Use_Command_Count = new System.Windows.Forms.Label();
+            this.L_Havent_Recovered_Yet_Count = new System.Windows.Forms.Label();
+            this.L_First_Select_Target = new System.Windows.Forms.Label();
+            this.L_Cannot_See_Target = new System.Windows.Forms.Label();
+            this.L_Cant_Cast_While_Stunned = new System.Windows.Forms.Label();
+            this.L_First_Select_Target_Count = new System.Windows.Forms.Label();
+            this.L_Cannot_See_Target_Count = new System.Windows.Forms.Label();
+            this.L_Cant_Cast_While_Stunned_Count = new System.Windows.Forms.Label();
+            this.L_Target_Too_Far_Away = new System.Windows.Forms.Label();
+            this.L_Target_Too_Far_Away_Count = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // B_Open_Log
+            // 
+            this.B_Open_Log.Location = new System.Drawing.Point(12, 12);
+            this.B_Open_Log.Name = "B_Open_Log";
+            this.B_Open_Log.Size = new System.Drawing.Size(75, 23);
+            this.B_Open_Log.TabIndex = 0;
+            this.B_Open_Log.Text = "Open Log";
+            this.B_Open_Log.UseVisualStyleBackColor = true;
+            this.B_Open_Log.Click += new System.EventHandler(this.B_Open_Log_Click);
+            // 
+            // L_Cant_Use_Command
+            // 
+            this.L_Cant_Use_Command.Location = new System.Drawing.Point(12, 82);
+            this.L_Cant_Use_Command.Margin = new System.Windows.Forms.Padding(3);
+            this.L_Cant_Use_Command.Name = "L_Cant_Use_Command";
+            this.L_Cant_Use_Command.Size = new System.Drawing.Size(130, 13);
+            this.L_Cant_Use_Command.TabIndex = 1;
+            this.L_Cant_Use_Command.Text = "Can\'t Use Command";
+            // 
+            // L_Havent_Recovered_Yet
+            // 
+            this.L_Havent_Recovered_Yet.Location = new System.Drawing.Point(12, 63);
+            this.L_Havent_Recovered_Yet.Margin = new System.Windows.Forms.Padding(3);
+            this.L_Havent_Recovered_Yet.Name = "L_Havent_Recovered_Yet";
+            this.L_Havent_Recovered_Yet.Size = new System.Drawing.Size(130, 13);
+            this.L_Havent_Recovered_Yet.TabIndex = 2;
+            this.L_Havent_Recovered_Yet.Text = "Haven\'t Recovered Yet";
+            // 
+            // L_Cant_Use_Command_Count
+            // 
+            this.L_Cant_Use_Command_Count.Location = new System.Drawing.Point(222, 82);
+            this.L_Cant_Use_Command_Count.Margin = new System.Windows.Forms.Padding(3);
+            this.L_Cant_Use_Command_Count.Name = "L_Cant_Use_Command_Count";
+            this.L_Cant_Use_Command_Count.Size = new System.Drawing.Size(50, 13);
+            this.L_Cant_Use_Command_Count.TabIndex = 3;
+            this.L_Cant_Use_Command_Count.Text = "0";
+            this.L_Cant_Use_Command_Count.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // L_Havent_Recovered_Yet_Count
+            // 
+            this.L_Havent_Recovered_Yet_Count.Location = new System.Drawing.Point(222, 63);
+            this.L_Havent_Recovered_Yet_Count.Margin = new System.Windows.Forms.Padding(3);
+            this.L_Havent_Recovered_Yet_Count.Name = "L_Havent_Recovered_Yet_Count";
+            this.L_Havent_Recovered_Yet_Count.Size = new System.Drawing.Size(50, 13);
+            this.L_Havent_Recovered_Yet_Count.TabIndex = 4;
+            this.L_Havent_Recovered_Yet_Count.Text = "0";
+            this.L_Havent_Recovered_Yet_Count.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // L_First_Select_Target
+            // 
+            this.L_First_Select_Target.Location = new System.Drawing.Point(12, 101);
+            this.L_First_Select_Target.Margin = new System.Windows.Forms.Padding(3);
+            this.L_First_Select_Target.Name = "L_First_Select_Target";
+            this.L_First_Select_Target.Size = new System.Drawing.Size(130, 13);
+            this.L_First_Select_Target.TabIndex = 5;
+            this.L_First_Select_Target.Text = "First Select Target";
+            // 
+            // L_Cannot_See_Target
+            // 
+            this.L_Cannot_See_Target.Location = new System.Drawing.Point(12, 120);
+            this.L_Cannot_See_Target.Margin = new System.Windows.Forms.Padding(3);
+            this.L_Cannot_See_Target.Name = "L_Cannot_See_Target";
+            this.L_Cannot_See_Target.Size = new System.Drawing.Size(130, 13);
+            this.L_Cannot_See_Target.TabIndex = 6;
+            this.L_Cannot_See_Target.Text = "Cannot See Target";
+            // 
+            // L_Cant_Cast_While_Stunned
+            // 
+            this.L_Cant_Cast_While_Stunned.Location = new System.Drawing.Point(12, 139);
+            this.L_Cant_Cast_While_Stunned.Margin = new System.Windows.Forms.Padding(3);
+            this.L_Cant_Cast_While_Stunned.Name = "L_Cant_Cast_While_Stunned";
+            this.L_Cant_Cast_While_Stunned.Size = new System.Drawing.Size(130, 13);
+            this.L_Cant_Cast_While_Stunned.TabIndex = 7;
+            this.L_Cant_Cast_While_Stunned.Text = "Can\'t Cast While Stunned";
+            // 
+            // L_First_Select_Target_Count
+            // 
+            this.L_First_Select_Target_Count.Location = new System.Drawing.Point(222, 101);
+            this.L_First_Select_Target_Count.Margin = new System.Windows.Forms.Padding(3);
+            this.L_First_Select_Target_Count.Name = "L_First_Select_Target_Count";
+            this.L_First_Select_Target_Count.Size = new System.Drawing.Size(50, 13);
+            this.L_First_Select_Target_Count.TabIndex = 8;
+            this.L_First_Select_Target_Count.Text = "0";
+            this.L_First_Select_Target_Count.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // L_Cannot_See_Target_Count
+            // 
+            this.L_Cannot_See_Target_Count.Location = new System.Drawing.Point(222, 120);
+            this.L_Cannot_See_Target_Count.Margin = new System.Windows.Forms.Padding(3);
+            this.L_Cannot_See_Target_Count.Name = "L_Cannot_See_Target_Count";
+            this.L_Cannot_See_Target_Count.Size = new System.Drawing.Size(50, 13);
+            this.L_Cannot_See_Target_Count.TabIndex = 9;
+            this.L_Cannot_See_Target_Count.Text = "0";
+            this.L_Cannot_See_Target_Count.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // L_Cant_Cast_While_Stunned_Count
+            // 
+            this.L_Cant_Cast_While_Stunned_Count.Location = new System.Drawing.Point(222, 139);
+            this.L_Cant_Cast_While_Stunned_Count.Margin = new System.Windows.Forms.Padding(3);
+            this.L_Cant_Cast_While_Stunned_Count.Name = "L_Cant_Cast_While_Stunned_Count";
+            this.L_Cant_Cast_While_Stunned_Count.Size = new System.Drawing.Size(50, 13);
+            this.L_Cant_Cast_While_Stunned_Count.TabIndex = 10;
+            this.L_Cant_Cast_While_Stunned_Count.Text = "0";
+            this.L_Cant_Cast_While_Stunned_Count.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // L_Target_Too_Far_Away
+            // 
+            this.L_Target_Too_Far_Away.Location = new System.Drawing.Point(12, 158);
+            this.L_Target_Too_Far_Away.Margin = new System.Windows.Forms.Padding(3);
+            this.L_Target_Too_Far_Away.Name = "L_Target_Too_Far_Away";
+            this.L_Target_Too_Far_Away.Size = new System.Drawing.Size(130, 13);
+            this.L_Target_Too_Far_Away.TabIndex = 11;
+            this.L_Target_Too_Far_Away.Text = "Target Too Far Away";
+            // 
+            // L_Target_Too_Far_Away_Count
+            // 
+            this.L_Target_Too_Far_Away_Count.Location = new System.Drawing.Point(222, 158);
+            this.L_Target_Too_Far_Away_Count.Margin = new System.Windows.Forms.Padding(3);
+            this.L_Target_Too_Far_Away_Count.Name = "L_Target_Too_Far_Away_Count";
+            this.L_Target_Too_Far_Away_Count.Size = new System.Drawing.Size(50, 13);
+            this.L_Target_Too_Far_Away_Count.TabIndex = 12;
+            this.L_Target_Too_Far_Away_Count.Text = "0";
+            this.L_Target_Too_Far_Away_Count.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.L_Target_Too_Far_Away_Count);
+            this.Controls.Add(this.L_Target_Too_Far_Away);
+            this.Controls.Add(this.L_Cant_Cast_While_Stunned_Count);
+            this.Controls.Add(this.L_Cannot_See_Target_Count);
+            this.Controls.Add(this.L_First_Select_Target_Count);
+            this.Controls.Add(this.L_Cant_Cast_While_Stunned);
+            this.Controls.Add(this.L_Cannot_See_Target);
+            this.Controls.Add(this.L_First_Select_Target);
+            this.Controls.Add(this.L_Havent_Recovered_Yet_Count);
+            this.Controls.Add(this.L_Cant_Use_Command_Count);
+            this.Controls.Add(this.L_Havent_Recovered_Yet);
+            this.Controls.Add(this.L_Cant_Use_Command);
+            this.Controls.Add(this.B_Open_Log);
+            this.Name = "MainScreen";
+            this.Text = "EQ Log Cleaner";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B_Open_Log;
+        private System.Windows.Forms.Label L_Cant_Use_Command;
+        private System.Windows.Forms.Label L_Havent_Recovered_Yet;
+        private System.Windows.Forms.Label L_Cant_Use_Command_Count;
+        private System.Windows.Forms.Label L_Havent_Recovered_Yet_Count;
+        private System.Windows.Forms.Label L_First_Select_Target;
+        private System.Windows.Forms.Label L_Cannot_See_Target;
+        private System.Windows.Forms.Label L_Cant_Cast_While_Stunned;
+        private System.Windows.Forms.Label L_First_Select_Target_Count;
+        private System.Windows.Forms.Label L_Cannot_See_Target_Count;
+        private System.Windows.Forms.Label L_Cant_Cast_While_Stunned_Count;
+        private System.Windows.Forms.Label L_Target_Too_Far_Away;
+        private System.Windows.Forms.Label L_Target_Too_Far_Away_Count;
+    }
+}
+
---
+++ EQ Log Cleaner/EQ Log Cleaner/MainScreen.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.CodeDom.Compiler;
+
+namespace EQ_Log_Cleaner
+{
+    public partial class MainScreen : Form
+    {
+        #region --- Variables -----------------------------------------------------
+
+        string ProgramVersion = "1.0.0.0";
+        string Title = "EQ Log Cleaner";
+
+        string Player;
+
+        string OriginalFileName = "";
+        string FileName = "";
+        TempFileCollection TempFiles = new TempFileCollection();
+
+        string Line;
+
+        #endregion
+
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void B_Open_Log_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+            OpenFileDialog1.InitialDirectory = Options.DefaultPath;
+
+            OpenFileDialog1.Filter = "EQ Log Files|*.txt;*.txt.gz";
+            OpenFileDialog1.Title = "Select an EverQuest Log File";
+
+            if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (CheckFile(OpenFileDialog1.FileName))
+                {
+
+                    LoadFile(OpenFileDialog1.FileName);
+                }
+            }
+        }
+
+        public bool CheckFile(string file)
+        {
+            bool FileGood = false;
+
+            if (File.Exists(file))
+            {
+                int found = 0;
+                for (int i = 0; i < file.Length; i++)
+                    if (file[i] == '\\')
+                        found = i + 1;
+
+//                Options.DefaultPath = file.Substring(0, found);
+
+                if (file.Contains("_"))
+                {
+                    found = file.IndexOf('_', found);
+                    if (file.Substring(found + 1, file.Length - found - 1).Contains("_"))
+                    {
+                        Player = file.Substring(found + 1, file.IndexOf('_', found + 1) - found - 1);
+                        FileName = file;
+                        FileGood = true;
+                    }
+                    else
+                        MessageBox.Show("Can only find one underscore in the filename, cannot determine character name.\n\nEither fill in the Player field on the Options Page, or have the\ncharacter name between two underscores in the file name\n\nFor example:\n\n   eqlog_Gamanern_maelin.txt   (ok)\n\n   eqlog_Gamanern_maelin_20070812.txt   (ok - and shows the date)\n\n   eqlog_Gamanern_maelin_Warden_Hanver.txt   (ok - and shows the target)\n\n   Warden_Hanvar_20070812.txt   (Will call character Hanvar)\n\n   Warden_Hanver.txt   (Only one underscore)\n\n   Warden-Hanver.txt   (No underscores)");
+                }
+                else
+                    MessageBox.Show("Cannot find any underscores in the filename, cannot determine character name.\n\nEither fill in the Player field on the Options Page, or have the\ncharacter name between two underscores in the file name\n\nFor example:\n\n   eqlog_Gamanern_maelin.txt   (ok)\n\n   eqlog_Gamanern_maelin_20070812.txt   (ok - and shows the date)\n\n   eqlog_Gamanern_maelin_Warden_Hanver.txt   (ok - and shows the target)\n\n   Warden_Hanvar_20070812.txt   (Will call character Hanvar)\n\n   Warden_Hanver.txt   (Only one underscore)\n\n   Warden-Hanver.txt   (No underscores)");
+            }
+            else
+                MessageBox.Show("Cannot find file: " + file + "\n\nYou have possibly renamed or deleted the file and a new file hasn't been created yet.");
+
+            return FileGood;
+        }
+
+        private void LoadFile(string file)
+        {
+            //Delete old temp files to prevent eating up space on the system.
+            TempFiles.Delete();
+            //Places the filename into a new string that doesn't change so that it can be referenced later, even if the filename itself changes.
+            OriginalFileName = FileName;
+
+            if (FileName.EndsWith(".gz"))
+            {
+                //Create a temporary file and place the name into a variable.
+                var fn = Path.GetTempFileName();
+                //Opens the selected file and puts it into a variable.
+                var backup = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+                //Puts the temp file into a variable
+                var temp = new FileStream(fn, FileMode.Create);
+                //Decompresses the selected file into a variable
+                var gzip = new GZipStream(backup, CompressionMode.Decompress);
+                //Copies the decompressed file into the temporary file.
+                gzip.CopyTo(temp);
+                gzip.Close();
+                temp.Close();
+                backup.Close();
+                //Adds the temporary file to the list of temporary files.
+                TempFiles.AddFile(fn, false);
+                //Places the filename into a new string that doesn't change so that it can be referenced later, even if the filename itself changes.
+                OriginalFileName = FileName;
+                //Set the filename to the temporary files' name.
+                FileName = fn;
+            }
+
+            int CantUseCommand = 0;
+            int HaventRecoveredYet = 0;
+            int FirstSelectTarget = 0;
+            int CannotSeeTarget = 0;
+            int CantCastWhileStunned = 0;
+            int TargetTooFarAway = 0;
+
+            //Create the streamreader and read the first two lines because the first line is headings.
+            StreamReader sr = new StreamReader(FileName, Encoding.UTF7);
+
+            Line = sr.ReadLine();
+
+            do
+            {
+                if (Line.Contains("You haven't recovered yet..."))
+                {
+                    HaventRecoveredYet = HaventRecoveredYet + 1;
+                }
+                else if (Line.Contains("You can't use that command right now..."))
+                {
+                    CantUseCommand = CantUseCommand + 1;
+                }
+                else if (Line.Contains("You must first select a target for this spell!"))
+                {
+                    FirstSelectTarget = FirstSelectTarget + 1;
+                }
+                else if (Line.Contains("You cannot see your target."))
+                {
+                    CannotSeeTarget = CannotSeeTarget + 1;
+                }
+                else if (Line.Contains("You can't cast spells while stunned!"))
+                {
+                    CantCastWhileStunned = CantCastWhileStunned + 1;
+                }
+                else if (Line.Contains("Your target is too far away, get closer!"))
+                {
+                    TargetTooFarAway = TargetTooFarAway + 1;
+                }
+
+                Line = sr.ReadLine();
+
+
+            } while (Line != null);
+
+            L_Cant_Use_Command_Count.Text = CantUseCommand.ToString();
+            L_Havent_Recovered_Yet_Count.Text = HaventRecoveredYet.ToString();
+            L_First_Select_Target_Count.Text = FirstSelectTarget.ToString();
+            L_Cannot_See_Target_Count.Text = CannotSeeTarget.ToString();
+            L_Cant_Cast_While_Stunned_Count.Text = CantCastWhileStunned.ToString();
+            L_Target_Too_Far_Away_Count.Text = TargetTooFarAway.ToString();
+        }
+
+    }
+}
